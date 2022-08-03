ea28358de73d372889b05b234712ca55707776ac
kevin knight <ksknight81@gmail.com>
2022-08-02 23:13:57 -0400

    added code snapshot

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,25 +1,24 @@
 async function upvoteClickHandler(event) {
-    event.preventDefault();
-    
-    const id = window.location.toString().split('/')[
-        window.locationtoString().split('/').length -1
-    ];
+  event.preventDefault();
 
-    const response = await fetch('/api/posts/upvote', {
-        method: 'PUT',
-        body: JSON.stringify({
-            post_id: id
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        document.location.reload();
-    } else {
-        alert(response.statusText);
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch('/api/posts/upvote', {
+    method: 'PUT',
+    body: JSON.stringify({
+      post_id: id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
